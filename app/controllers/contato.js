0deaa2649590662a3b1f0aadd664dbcee95ca318
Victor <tietata.ninja@gmail.com>
2022-03-24 22:00:20 -0300

    yoooo

--- app/controllers/contato.js
+++ app/controllers/contato.js
@@ -9,25 +9,25 @@ var contatos = [
  {_id: 8, nome: 'Ben Dover', email: 'Ben@ifsp.edu.br'}
 ]
 
-module.exports = function(){
-	var controller = {};
-	controller.listaContatos = function(req, res){
-		res.json(contatos);
-	};
-	controller.obtemContato = function(req, res) {
-		console.log('Selecionou o contato: ' + req.params.id);
-		var idContato = req.params.id;
-		var contato = contatos.filter(function(contato){
-			return contato._id == idContato;
-		})[0];
-		contato ? res.json(contato) : res.status(404).send('Contato não encontrado!');
-	};
-	controller.removeContato = function(req, res) {
-		var idContato = req.params.id;
-		contatos = contatos.filter(function(contato) {
-			return contato_id != idContato;
-		});
-		res.send(204).end();
-	}
-	return controller;
+module.exports = function() {
+    var controller = {};
+    controller.listaContatos = function(req, res) {
+        res.json(contatos);
+    };
+    controller.obtemContato = function(req, res) {
+        console.log('Selecionou o contato: ' + req.params.id);
+        var idContato = req.params.id;
+        var contato = contatos.filter(function(contato) {
+            return contato._id == idContato;
+        })[0];
+        contato ? res.json(contato) : res.status(404).send('Contato não encontrado!');
+    };
+    controller.removeContato = function(req, res) {
+        var idContato = req.params.id;
+        contatos = contatos.filter(function(contato) {
+            return contato._id != idContato;
+        });
+        res.send(204).end();
+    };
+    return controller;
 };
