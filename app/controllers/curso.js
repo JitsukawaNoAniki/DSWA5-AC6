2a6598fa8b7788cc73b06d135349a211be453341
Victor Jitsukawa <victor.jitsukawa@aluno.ifsp.edu.br>
2022-03-30 14:17:51 -0300

    add examples

--- app/controllers/curso.js
+++ app/controllers/curso.js
@@ -2,7 +2,9 @@ var cursos = [
     { _id: 1, curso: 'Engenharia de Produção', coordenador: 'fabio.teixeira@ifsp.edu.br' },
     { _id: 2, curso: 'Tecnologia em Análise e Desenvolvimento de Sistemas', coordenador: 'fabiano.teixeira@ifsp.edu.br' },
     { _id: 3, curso: 'Licenciatura em Letras Português/Inglês', coordenador: 'melissa.teixeira@ifsp.edu.br' },
-    { _id: 4, curso: 'Tecnologia em Gestão Pública', coordenador: 'melissa.teixeira@ifsp.edu.br' }
+    { _id: 4, curso: 'Astrologia', coordenador: 'Tomas@ifsp.edu.br' },
+    { _id: 4, curso: 'Óleos essenciais', coordenador: 'Mike@ifsp.edu.br' },
+    { _id: 4, curso: 'Antivaxx', coordenador: 'Hugh@ifsp.edu.br' }
 ]
 
 module.exports = function() {
